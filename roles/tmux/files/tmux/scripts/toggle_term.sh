f98c277a894db1a0f8d1b4328ad9dd9d6727e934
Oleg Levantin <stounfo@gmail.com>
2024-05-05 00:28:21 +0400

    Improve floating terminal in tmux

--- roles/tmux/files/tmux/scripts/toggle_term.sh
+++ roles/tmux/files/tmux/scripts/toggle_term.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# source: https://gist.github.com/pbnj/67c16c37918ba40bbb233b97f3e38456
+# idea: https://gist.github.com/pbnj/67c16c37918ba40bbb233b97f3e38456
 
 set -uo pipefail
 
-POPUP_SESSION_NAME="Float$(tmux display-message -p '#{pane_current_path}')"
 
-if [ "$(tmux display-message -p -F "#{session_name}")" = $POPUP_SESSION_NAME ];then
-  tmux -L float detach-clien
+if [[ "$(tmux display-message -p -F "#{session_name}")" == Float* ]]; then
+    tmux -L float detach-clien
 else
-  tmux popup -d '#{pane_current_path}' -xC -yC -w91% -h80% -E "tmux -L float attach -t $POPUP_SESSION_NAME || tmux -L float -f ~/.config/tmux/float.conf new -s $POPUP_SESSION_NAME"
+    POPUP_SESSION_NAME="Float/$(tmux display-message -p '#{window_id}')"
+    tmux popup -d '#{pane_current_path}' -xC -yC -w91% -h80% -E "tmux -L float attach -t $POPUP_SESSION_NAME || tmux -L float -f ~/.config/tmux/float.conf new -s $POPUP_SESSION_NAME"
 fi
