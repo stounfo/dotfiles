edb53ce4bd8a98a9ccd5a038b50144449612138b
Oleg Levantin <stounfo@gmail.com>
2024-09-11 22:28:00 +0400

    Fix typo

--- roles/tmux/files/tmux/scripts/toggle_term.sh
+++ roles/tmux/files/tmux/scripts/toggle_term.sh
@@ -5,7 +5,7 @@ set -uo pipefail
 
 
 if [[ "$(tmux display-message -p -F "#{session_name}")" == Float* ]]; then
-    tmux -L float detach-clien
+    tmux -L float detach-client
 else
     POPUP_SESSION_NAME="Float/$(tmux display-message -p '#{window_id}')"
     tmux popup -d '#{pane_current_path}' -xC -yC -w91% -h80% -E "tmux -L float attach -t $POPUP_SESSION_NAME || tmux -L float -f ~/.config/tmux/float.conf new -s $POPUP_SESSION_NAME"
